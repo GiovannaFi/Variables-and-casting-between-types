212ed77ea748313597ab3d309b93ef222337e7e6
gio <ziovanava@gmail.com>
2022-12-27 12:36:21 +0100

    Kotlin2

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val number1 : Long = 300000
+    val number2 : Short = 1000
+    val number3 = 20.8
+    val result = number1 + number2 + number3
+    println("result: $result")
+}
